85f5592cfc65804ac4b8172f9be43771cdb60b10
Pedro Sanders <sanderspedro@gmail.com>
2021-11-05 00:13:55 -0400

    Fixed fonts + cleanup

--- www/next.config.js
+++ www/next.config.js
@@ -3,6 +3,6 @@ const withMDX = require('@next/mdx')()
 module.exports = withMDX({
   pageExtensions: ['js', 'jsx', 'tsx', 'md', 'mdx'],
   images: {
-    domains: ['github.com', 'ca.slack-edge.com', 'res.cloudinary.com'],
+    domains: ['github.com'],
   },
 })
